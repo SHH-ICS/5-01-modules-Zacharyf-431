14e38a87ce8586f3405c1c0a06b5a3d8fb1e6f87
Zachary Fowler <zachary.fowler1@stu.ocsb.ca>
2024-11-20 14:53:03 +0000

    fgh

--- Q3Addition.py
+++ Q3Addition.py
@@ -1,2 +1,16 @@
 # Create a program that will ask the user an addition question. 
 # The program will generate two random numbers between 1 and 100, and display them as an addition question with appropriate prompts.
+
+import random
+x = random.randint(1,101)
+y = random.randint(1,101)
+print ("What is the sum of these two numbers?",   x, "+", y) 
+try:
+    z = x + y
+    a = int(input())
+    if a == int(z):
+        print ("That is correct. You get a Cookie!!!!")
+    else:
+        print ("You are incorrect. No Coookie for you!!!!")
+except ValueError:
+    print ("Put in a valid number")
--- Q4Picture.py
+++ Q4Picture.py
@@ -1,2 +1,13 @@
 # Using the pygame library, draw a simple picture. 
 # It can be anything you like, but you must use at least 2 different types of shapes and 3 different colors.
+import random
+x = random.randint(1,101)
+y = random.randint(1,101)
+print ("What is the sum of these two numbers?",   x, "+", y) 
+z = x + y
+a = int(input())
+
+for i in range (int(a)):
+    if str(z)== i:
+        print ("y")
+        break

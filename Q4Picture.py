ff574d9354b5f896002a661eef7eb43a2a9fe4bc
Zachary Fowler <zachary.fowler1@stu.ocsb.ca>
2024-11-22 17:04:44 +0000

    ju

--- Q4Picture.py
+++ Q4Picture.py
@@ -1,13 +1,36 @@
-# Using the pygame library, draw a simple picture. 
-# It can be anything you like, but you must use at least 2 different types of shapes and 3 different colors.
-import random
-x = random.randint(1,101)
-y = random.randint(1,101)
-print ("What is the sum of these two numbers?",   x, "+", y) 
-z = x + y
-a = int(input())
+import pygame
 
-for i in range (int(a)):
-    if str(z)== i:
-        print ("y")
-        break
+from pygame import Color, Rect
+from pygame import draw
+from pygame import display
+
+SCREEN_SIZE = (500, 500)
+
+
+pygame.init()
+
+
+gameDisplay = display.set_mode(SCREEN_SIZE)
+
+
+gameDisplay.fill(Color('lightblue'))
+
+
+
+
+draw.rect(gameDisplay, Color('red'), Rect(50, 50, 45, 300))
+
+draw.rect(gameDisplay, Color('red'), Rect(200, 50, 45, 300))
+
+draw.rect(gameDisplay, Color('red'), Rect(50, 180, 150, 45))
+
+draw.rect(gameDisplay, Color('blue'), Rect(350, 200, 45, 150))
+
+draw.circle(gameDisplay, Color('gold'), (370, 130),30)
+
+
+
+display.flip()
+
+
+input("Press enter to exit")
